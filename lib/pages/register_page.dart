1017c0c503acc3d01e2183de6dc2f8b82ae3c255
hideokawahara <kawahide.72@gmail.com>
2022-01-23 22:00:04 +0900

    feat: 画像選択実装

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -12,6 +12,7 @@ import 'package:chatify_app/services/cloud_storage_service.dart';
 //Widgets
 import 'package:chatify_app/widgets/custom_input_fields.dart';
 import 'package:chatify_app/widgets/rounded_button.dart';
+import 'package:chatify_app/widgets/rounded_image.dart';
 
 //Providers
 import 'package:chatify_app/providers/authentication_provider.dart';
@@ -22,12 +23,70 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  PlatformFile? _profileImage;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.03,
+          vertical: _deviceHeight * 0.02,
+        ),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profileImageField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImageField() {
+    return GestureDetector(
+      onTap: () {
+        GetIt.instance.get<MediaService>().pickImageFromLibrary().then(
+          (_file) {
+            setState(
+              () {
+                _profileImage = _file;
+              },
+            );
+          },
+        );
+      },
+      child: () {
+        if (_profileImage != null) {
+          return RoundedImageFile(
+            key: UniqueKey(),
+            image: _profileImage!,
+            size: _deviceHeight * 0.15,
+          );
+        } else {
+          return RoundedImageNetwork(
+            key: UniqueKey(),
+            imagePath:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBpz05z7EfdZsc0fH_D0HN0TiVwzrwqBldrg&usqp=CAU",
+            size: _deviceHeight * 0.15,
+          );
+        }
+      }(),
+    );
   }
 }
