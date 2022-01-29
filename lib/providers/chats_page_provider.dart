cc8dc0d6ebf3df009242ab660dc14544677b757a
hideokawahara <kawahide.72@gmail.com>
2022-01-29 20:36:19 +0900

    feat: chat stream作成

--- lib/providers/chats_page_provider.dart
+++ lib/providers/chats_page_provider.dart
@@ -36,5 +36,52 @@ class ChatsPageProvider extends ChangeNotifier {
     super.dispose();
   }
 
-  void getChats() async {}
+  void getChats() async {
+    try {
+      _chatsStream =
+          _db.getChatsForuser(_auth.user.uid).listen((_snapshot) async {
+        chats = await Future.wait(
+          _snapshot.docs.map(
+            (_d) async {
+              Map<String, dynamic> _chatData =
+                  _d.data() as Map<String, dynamic>;
+
+              List<ChatUser> _members = [];
+              for (var _uid in _chatData["members"]) {
+                DocumentSnapshot _userSnapshot = await _db.getUser(_uid);
+                Map<String, dynamic> _userData =
+                    _userSnapshot.data() as Map<String, dynamic>;
+                _members.add(
+                  ChatUser.fromJSON(_userData),
+                );
+              }
+
+              List<ChatMessage> _messages = [];
+              QuerySnapshot _chatMessage =
+                  await _db.getLastMessageForChat(_d.id);
+              if (_chatMessage.docs.isNotEmpty) {
+                Map<String, dynamic> _messageData =
+                    _chatMessage.docs.first.data()! as Map<String, dynamic>;
+                ChatMessage _message = ChatMessage.from(_messageData);
+                _messages.add(_message);
+              }
+
+              return Chat(
+                uid: _d.id,
+                currentUserUid: _auth.user.uid,
+                members: _members,
+                messages: _messages,
+                activity: _chatData["is_activity"],
+                group: _chatData["is_group"],
+              );
+            },
+          ).toList(),
+        );
+        notifyListeners();
+      });
+    } catch (e) {
+      print("Error getting chats");
+      print(e);
+    }
+  }
 }
