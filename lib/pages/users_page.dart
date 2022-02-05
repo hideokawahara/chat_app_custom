91dce34861a993dc7e819630d843911d50cffdf2
hideokawahara <kawahide.72@gmail.com>
2022-02-05 22:45:46 +0900

    feat: トップバー追加

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -3,20 +3,69 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
+//Providers
+import 'package:chatify_app/providers/authentication_provider.dart';
+
+//Widgets
+import 'package:chatify_app/widgets/top_bar.dart';
+import 'package:chatify_app/widgets/custom_input_fields.dart';
+import 'package:chatify_app/widgets/custom_list_view_tiles.dart';
+import 'package:chatify_app/widgets/rounded_button.dart';
+
+//Models
+import 'package:chatify_app/models/chat_user.dart';
+
 class UsersPage extends StatefulWidget {
   @override
   _UsersPageState createState() => _UsersPageState();
 }
 
 class _UsersPageState extends State<UsersPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
+
+  final TextEditingController _searchFieldTextEditingController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold(
-      backgroundColor: Colors.green,
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TopBar(
+            'ユーザー',
+            primaryAction: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.logout,
+                  color: Color.fromRGBO(
+                    0,
+                    82,
+                    218,
+                    1.0,
+                  ),
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
