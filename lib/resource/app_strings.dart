d61cdbb4fff43b62133960a061466407ccf3f2c8
hideokawahara <kawahide.72@gmail.com>
2022-02-08 22:40:45 +0900

    feat: 文字列管理ファイル作成

---
+++ lib/resource/app_strings.dart
@@ -0,0 +1,26 @@
+class AppStrings {
+  //HomePage
+  static const String navigationTitleChats = "会話";
+  static const String navigationTitleUsers = "友達";
+
+  //ChatsPage
+  static const String chatsPageTitle = "メッセージ";
+  static const String emptyChatsText = "メッセージがありません";
+  static const String imageSentText = "画像を送信しました";
+
+  //ChatPage
+  static const String encourageGreetingsText = "挨拶しよう";
+  static const String regExForMessage = r"^(?!\s*$).+";
+  static const String inputHintText = "入力";
+
+  //UsersPage
+  static const String usersPageTitle = "ユーザー";
+  static const String searchInputHintText = "ユーザーを探す";
+  static const String lastActiveText = "最終ログイン: ";
+  static const String noFoundUsersText = "そんな人はいません";
+  static const String talkWithText = "と話す";
+  static const String createGroup = "グループを作成する";
+
+  //common
+  static const String isEmptyText = "";
+}
